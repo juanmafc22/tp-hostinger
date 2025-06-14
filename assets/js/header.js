bda4f4724aa61540ec8632d57e02f5ad3d7ef346
Juan Manuel Fernandez <juanmafc22@gmail.com>
2025-06-14 13:27:41 -0300

    Arreglo width del header. Arreglo de los media queries

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,29 @@
+/*
+El script usa querySelector, lo que implica que tengo que agregar una 
+class, en este caso .header para que se guarde en la const header
+intento cambiar por getElementById para no tener el duplicado
+header
+    .header {
+    }
+en el css
+
 document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header');
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 0) {
+            header.classList.add('header--scrolled');
+        } else {
+            header.classList.remove('header--scrolled');
+        }
+    });
+});
+
+funcionó, great!
+
+*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.getElementById('header');
     window.addEventListener('scroll', function () {
         if (window.scrollY > 0) {
             header.classList.add('header--scrolled');
